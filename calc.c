663ad21305102ee5798500adf2f854f2a6662931
T2us <44253504+T2us@users.noreply.github.com>
2018-12-13 18:02:06 +0900

    Update calc.c

--- calc.c
+++ calc.c
@@ -18,7 +18,7 @@ void Icalc(int input)
             char math1, math2, math3;
             printf("\tYou have selected three-dimension form.\n\tPlease enter the three-dimension form.(ex.3x^3+2x^2+3x+1)\n>>");
             scanf("%fx^%d%c%fx^%d%c%fx%c%d", &c_3_1, &c_3_2, &math1, &c_2_1, &c_2_2, &math2, &c_1, &math3, &c_0);
-            printf("\n------------------------\n||\t\%.2fx^%d%c%.2fx^%d%c%.2f^2+c\t\t||\n------------------------\n", (c_3_1/(c_3_2+1)), (c_3_2+1), math1, (c_2_1/(c_2_2+1)), (c_2_2+1), math2, (c_1/2));
+            printf("\n------------------------------------------------\n||\t\%.2fx^%d %c %.2fx^%d %c %.2f^2 + c|\n------------------------------------------------\n", (c_3_1/(c_3_2+1)), (c_3_2+1), math1, (c_2_1/(c_2_2+1)), (c_2_2+1), math2, (c_1/2));
             break;
         }
         case 2:
@@ -27,8 +27,8 @@ void Icalc(int input)
             int c_4_2 ,c_3_2, c_2_2, c_0=0;
             char math1, math2, math3, math4;
             printf("\tYou have selected four-dimension form.\n\tPlease enter the four-dimension form.(ex.4x^4+3x^3+2x^2+3x+1)\n>>");
-            scanf("%fx^%d%c%fx^%d%c%fx^%d%c%fx^%d%c%fx%c%d",&c_4_1, &c_4_2, &math1, &c_3_1, &c_3_2, &math2, &c_2_1, &c_2_2, &math3, &c_1, &math4, &c_0);
-            printf("\n------------------------\n||\t\%.2fx^%d%c%.2fx^%d%c%.2fx^%d%c%.2f^2+c\t\t||\n------------------------\n",(c_4_1/(c_4_2+1)), (c_4_2+1), math1, (c_3_1/(c_3_2+1)), (c_3_2+1), math2, (c_2_1/(c_2_2+1)), (c_2_2+1), math3, (c_1/2));
+            scanf("%fx^%d %c%fx^%d%c%fx^%d%c%fx^%d%c%fx%c%d",&c_4_1, &c_4_2, &math1, &c_3_1, &c_3_2, &math2, &c_2_1, &c_2_2, &math3, &c_1, &math4, &c_0);
+            printf("\n------------------------------------------------\n||\t\%.2fx^%d %c %.2fx^%d %c %.2fx^%d %c %.2f^2 + c\n------------------------------------------------\n",(c_4_1/(c_4_2+1)), (c_4_2+1), math1, (c_3_1/(c_3_2+1)), (c_3_2+1), math2, (c_2_1/(c_2_2+1)), (c_2_2+1), math3, (c_1/2));
             break;
         }
         case 3:
@@ -53,7 +53,7 @@ void Dcalc(int input)
             char math1, math2, math3;
             printf("\tYou have selected three-dimension form.\n\tPlease enter the three-dimension form.(ex.3x^3+2x^2+3x+1)\n>>");
             scanf("%dx^%d%c%dx^%d%c%dx%c%d", &c_3_1, &c_3_2, &math1, &c_2_1, &c_2_2, &math2, &c_1, &math3, &c_0);
-            printf("\n------------------------\n||\t\%dx^%d%c%dx^%d%c%d\t\t||\n------------------------\n", (c_3_1*c_3_2), (c_3_2-1), math1, (c_2_1*c_2_2), (c_2_2-1), math2, c_1);
+            printf("\n------------------------------------------------\n||\t\%dx^%d %c %dx^%d %c %d\n------------------------------------------------\n", (c_3_1*c_3_2), (c_3_2-1), math1, (c_2_1*c_2_2), (c_2_2-1), math2, c_1);
             break;
         }
         case 2:
@@ -62,7 +62,7 @@ void Dcalc(int input)
             char math1, math2, math3, math4;
             printf("\tYou have selected four-dimension form.\n\tPlease enter the four-dimension form.(ex.4x^4+3x^3+2x^2+3x+1)\n>>");
             scanf("%dx^%d%c%dx^%d%c%dx^%d%c%dx%c%d", &c_4_1, &c_4_2, &math1, &c_3_1, &c_3_2, &math2, &c_2_1, &c_2_2, &math3, &c_1, &math4, &c_0);
-            printf("\n------------------------\n||\t\%dx^%d%c%dx^%d%c%dx%c%d\t\t||\n------------------------\n", (c_4_1*c_4_2), (c_4_2-1), math1, (c_3_1*c_3_2), (c_3_2-1), math2, (c_2_1*c_2_2), math3, c_1);
+            printf("\n------------------------------------------------\n||\t\%dx^%d %c %dx^%d %c %dx %c %d\n------------------------------------------------\n", (c_4_1*c_4_2), (c_4_2-1), math1, (c_3_1*c_3_2), (c_3_2-1), math2, (c_2_1*c_2_2), math3, c_1);
             break;
         }
         case 3:
@@ -71,7 +71,7 @@ void Dcalc(int input)
             char math1, math2, math3, math4, math5;
             printf("\tYou have selected five-dimension form.\n\tPlease enter the five-dimension form.(ex.5x^5+4x^4+3x^3+2x^2+3x+1)\n>>");
             scanf("%dx^%d%c%dx^%d%c%dx^%d%c%dx^%d%c%dx%c%d", &c_5_1, &c_5_2, &math1, &c_4_1, &c_4_2, &math2, &c_3_1, &c_3_2, &math3, &c_2_1, &c_2_2, &math4, &c_1, &math5, &c_0);
-            printf("\n------------------------\n||\t\%dx^%d%c%dx^%d%c%dx^%dx%c%d\t\t||\n------------------------\n", (c_5_1*c_5_2), (c_5_2-1), math1, (c_4_1*c_4_2), (c_4_2-1), math2, (c_3_1*c_3_2), (c_3_2-1), math3, (c_2_1*c_2_2), math4, c_1);
+            printf("\n------------------------------------------------\n||\t\%dx^%d %c %dx^%d %c %dx^%dx %c %d\n------------------------------------------------\n", (c_5_1*c_5_2), (c_5_2-1), math1, (c_4_1*c_4_2), (c_4_2-1), math2, (c_3_1*c_3_2), (c_3_2-1), math3, (c_2_1*c_2_2), math4, c_1);
             break;
         }
             exit(1);
@@ -95,7 +95,7 @@ void calc(int input1, char math, int input2)
             Answer = input1 / input2;
             break;
     }
-    printf("\n------------------------\n||\t\t%d %c %d = %f ||\n------------------------\n", input1, math, input2, Answer);
+    printf("\n------------------------\n||\t\t%d %c %d = %.2f\n------------------------\n", input1, math, input2, Answer);
     exit(1);
 }
 
@@ -104,6 +104,7 @@ int main()
 {
     int input, input1, input2=0;
     char math;
+
     while(1)
     {
         if(fork() == 0)
